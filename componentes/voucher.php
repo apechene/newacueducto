d0b316dbe135b25045aae4c34682a7225ec8381e
apechene <45855745+apechene@users.noreply.github.com>
2025-02-25 09:31:56 -0500

    Update voucher.php
    cambios de informacion

--- componentes/voucher.php
+++ componentes/voucher.php
@@ -1,59 +1,63 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-
+<!DOCTYPE html>
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket de Venta</title>
+    <style>
 
-	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-	
-	<title>Puntored</title>
-	
+	    
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 20px;
+            background-color: #f8f8f8;
+        }
+        .ticket {
+            width: 250px;
+            background: white;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+            margin: auto;
+        }
+        .ticket h1 {
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .ticket p {
+            font-size: 14px;
+            margin: 5px 0;
+        }
+        .ticket .bold {
+            font-weight: bold;
+        }
+        .separator {
+            border-top: 1px dashed black;
+            margin: 10px 0;
+        }
+    </style>
 
-    <link rel='stylesheet' type='text/css' href='./ccs/styles.css' />
-	<link rel='stylesheet' type='text/css' href='./ccs/print.css' media="print" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 	<link href="https://fonts.googleapis.com/css?family=Cantarell&display=swap" rel="stylesheet">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.css"/>
-
-</head>
-<?php
+	<?php
 require_once "../php/conexion.php";
 $connect=conexion(); 
 if(isset($_POST["id15"]))  
 {  
 	 $output = '';  
 	
-?><body >
-
+?>
 
 <?php
 		
 $id=$_POST['id15'];
 
 ?>
-
-<body>
-
-
-
-
-
-
-	<div id="printableArea">
-<div class="ticket">
-
-	<div id="page-wrap">
-
-		<h5 align="center"> TRANSACCIÓN EXITOSA</h5>
 	
-
-
-		</body>
-            <table style=" border: 0px !important;">
-                
-                    <td>Fecha    </td>
-                    <td><?php
-			$consulta=("SELECT `idconsumo_factura`,`Valor Recaudado`,`Fecha` FROM `recaudo` WHERE `idRecaudo` ='".$id."'");
-			$sql=mysqli_query($connect,$consulta);
+</head>
+<body>
+	<?php
+	$consulta=("SELECT `idconsumo_factura`,`Valor Recaudado`,`Fecha` FROM `recaudo` WHERE `idRecaudo` ='".$id."'");
+	$sql=mysqli_query($connect,$consulta);
 while($row=mysqli_fetch_array($sql)){
 
     $fecha= $row['Fecha'];
@@ -64,123 +68,24 @@ echo $row['Fecha']; }
 
 
 
-					  ?></td>
-                
-				<tr>
-                    <td>HORA    </td>
-					<?php $hoy=date('H:i');?>
-                    <td><?php echo $hoy?></td>
-                </tr>
+					  ?>
 
-
-                <tr>
-                <td>Terminal     </td>
-
-                 <td>268333     </td>
-
-                </tr>
-
-                <tr>
-                <td>Convrenio     </td>
-
-                 <td>ACUEDUCTO VEREDAL      </td>
-
-                </tr>
-
-                <tr>
-                <td>Cod. Convenio     </td>
-
-                 <td>001       </td>
-
-                </tr>
-
-                <tr>
-                <td>Comercio     </td>
-
-                 <td>355583     </td>
-
-                </tr>
-
-                <tr>
-                <td>Aprobación sistema     </td>
-                <?php $six = random_int(100000, 999999);?>
-                 <td><?php echo $id?>    </td>
-
-                </tr>
-
-                <tr>
-                <td>Factura pagada     </td>
-
-                 <td><?php echo $factura ?>       </td>
-
-                </tr>
-
-                <tr>
-                <td>Valor     </td>
-
-                 <td>$ <?php echo $Valor?>      </td>
-
-                </tr>
-
-                <tr>
-                <td>Usuario     </td>
-
-                 <td>Rv5sistem    </td>
-
-                </tr>
-				</table>
-
-   <div>
-	   <h3> Consulta Datos financieros del Acueducto 
-		   y une a nuestro Canal en WhatsApp usando el enlace bit.ly/m/sanroque </h3>
-	   Escanea el QR: 
-<div class="alinear-img">
-<img src="./img/qrs.png" alt="QR Code" width="200" height="200">
-
+<div class="ticket">
+    <h3>Acueducto San Roque</h3>
+    <p class="bold">Comprobante de Recaudo</p>
+    <p><span class="bold">Fecha:</span><?php echo date('j-m-y');?></p>
+    <p><span class="bold">Hora:</span><?php echo date("H:i:s"); ?></p>
+    <p><span class="bold">Factura:</span> <?php echo $factura ?> </p>
+    <p><span class="bold">Cod. exito.:</span> <?php echo $id?> </p>
+    <p><span class="bold">Valor:</span> $<?php echo $Valor?> </p>
+    <div class="separator"></div>
+    <p>Gracias por tu Pago!</p>
+    <p></p>
 </div>
-   </div>
-
-		<div align="center" class="pago2">
-		  <h2>
-          Sistema de información
-          Acueducto San Roque
-          Conserve esta tirilla-
-          Es su unico soporte de Pago. 
-          2024
-
-</h2>
-		</div>
-
-			
-			
-			
-                   
-				  
-		<?php	
-			
-
-
 	
-}
 
-
-
-			
-			 ?>	 
-			 
-	
-	
-			
-	</div>				 
-		
-		
-		</div>
-		
 </body>
-
-
 </html>
-
 
 <script>
     window.print();
